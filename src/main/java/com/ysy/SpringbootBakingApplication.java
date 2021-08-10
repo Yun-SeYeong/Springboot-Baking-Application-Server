22c0cc30b8f66d64f124685a173707e0c3a0fe4e
Yun-SeYeong <dbstpdud09@naver.com>
2021-08-10 15:02:43 +0900

    [INIT]

---
+++ src/main/java/com/ysy/SpringbootBakingApplication.java
@@ -0,0 +1,13 @@
+package com.ysy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBakingApplication.class, args);
+	}
+
+}
